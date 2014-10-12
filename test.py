d453c4d72856d8b0f0ae971d1dafd687b5325db5
spline <reticulatingspline@github.com>
2014-10-12 15:13:42 -0400

    Fix tests.

--- test.py
+++ test.py
@@ -7,12 +7,12 @@
 
 from supybot.test import *
 
-class HardballTestCase(PluginTestCase):
+class HardballTestCase(ChannelPluginTestCase):
     plugins = ('Hardball',)
 
     def testHardball(self):
-        self.assertResponse('hardballchannel add #test NYY', "ERROR: '#test' is not a valid channel. You must add a channel that we are in.")
-        self.assertResponse('hardballchannel del #test NYY', "ERROR: I do not have NYY in #test")
+        self.assertResponse('hardballchannel add #test NYY', "I have added NYY into #test")
+        self.assertResponse('hardballchannel del #test NYY', "I have successfully removed NYY from #test")
 
 
 # vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
