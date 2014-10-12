18a950e375ef085b9da9e6bdcb9d6323209a1668
spline <reticulatingspline@github.com>
2014-10-12 11:52:28 -0400

    Add in tests.

--- test.py
+++ test.py
@@ -10,5 +10,9 @@ from supybot.test import *
 class HardballTestCase(PluginTestCase):
     plugins = ('Hardball',)
 
+    def testHardball(self):
+        self.assertResponse('hardballchannel add #test NYY', "ERROR: '#test' is not a valid channel. You must add a channel that we are in.")
+        self.assertResponse('hardballchannel del #test NYY', "ERROR: I do not have NYY in #test")
+
 
 # vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
